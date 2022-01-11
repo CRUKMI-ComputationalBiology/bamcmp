4c8f80bb0e6b3142a3b51cf4bee1ca4b134f152d
Marek Dynowski <marek.dynowski@manchester.ac.uk>
2022-01-11 14:53:02 +0000

    <getopt.h> now explicitly included in bamcmp.cpp as versions getopt() has been moved in it's own file in the recent version.
    FYI: Make process produced the following error:
    
    error: 'getopt' was not declared in this scop
    
    Now fixed.

--- src/bamcmp.cpp
+++ src/bamcmp.cpp
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <iostream>
 #include <vector>
 #include <string>
+#include <getopt.h>
 
 #include <htslib/hts.h>
 #include <htslib/sam.h>
